ede88d8f017913b9b7ff9f2ff7971e445a8a2c7f
findingflow <kennedymj97@gmail.com>
2019-07-29 16:02:03 +0100

    project 8 complete

--- projects/vm/cmd/main.go
+++ projects/vm/cmd/main.go
@@ -12,21 +12,15 @@ import (
 )
 
 func main() {
+	// set args to all the args after filename
 	args := os.Args[1:]
-	file, err := os.Open(args[0])
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer func() {
-		if err := file.Close(); err != nil {
-			log.Fatal(err)
-		}
-	}()
 
-	dirname, filename := filepath.Split(args[0])
-	fnameNoExt := strings.Replace(filename, ".vm", "", 1)
-	outFilename := strings.Replace(filename, ".vm", ".asm", 1)
-	outFilePath := dirname + outFilename
+	// check if the first arg is a directory
+	path := filepath.Clean(args[0])
+	isPathDir, err := isDirectory(path)
+
+	// create output filestream
+	outFilePath := createOutputPath(path, isPathDir)
 	outFile, err := os.Create(outFilePath)
 	if err != nil {
 		log.Fatal(err)
@@ -36,9 +30,75 @@ func main() {
 			log.Fatal(err)
 		}
 	}()
+
 	w := bufio.NewWriter(outFile)
 	defer func() {
 		if err := w.Flush(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	
+	// write init for output file
+	var aw translater.Translater
+	aw = &translater.AssemblyWriter{
+		Filename: "",
+		FunctionName: "",
+		CommandType: "",
+		Arg1: "Sys.init",
+		Arg2: 0,
+	}
+	w.WriteString(aw.WriteInit())
+
+	// if a directory process all files in directory else just process file
+	if isPathDir {
+		var files []string
+		err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+			files = append(files, path)
+			return nil
+		})
+		if err != nil {
+			panic(err)
+		}
+
+		for _, file := range files {
+			if filepath.Ext(file) == ".vm" {
+				processFile(file, w)
+			}
+		}
+	} else {
+		processFile(path, w)
+	}
+}
+
+func isDirectory(path string) (bool, error) {
+	fileInfo, err := os.Stat(path)
+	if err != nil{
+		return false, err
+	}
+	return fileInfo.IsDir(), err
+}
+
+func createOutputPath(path string, isDir bool) string {
+	dirname, filename := filepath.Split(path)
+	outFilename := strings.Replace(filename, ".vm", "", 1)
+	var outPath string
+	if isDir {
+		outPath = path + "/" + filename + ".asm"
+	} else {
+		outPath = dirname + outFilename + ".asm"
+	}
+	return outPath
+}
+
+func processFile(path string, w *bufio.Writer) {
+	fname := filepath.Base(path)
+	fnameNoExt := strings.Replace(fname, ".vm", "", 1)
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
 			log.Fatal(err)
 		}
 	}()
@@ -46,6 +106,7 @@ func main() {
 	var p parser.VmParser
 	var aw translater.Translater
 	equalityCheckCount := 0
+	functionName := ""
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
@@ -69,8 +130,13 @@ func main() {
 			log.Fatal(err)
 		}
 
+		if commandType == "C_FUNCTION" {
+			functionName = firstArg
+		}
+
 		aw = &translater.AssemblyWriter{
 			Filename:    fnameNoExt,
+			FunctionName: functionName,
 			CommandType: commandType,
 			Arg1:        firstArg,
 			Arg2:        secondArg,
@@ -89,6 +155,12 @@ func main() {
 			assemblyCode = aw.WriteGoto()
 		case "C_IF":
 			assemblyCode = aw.WriteIf()
+		case "C_FUNCTION":
+			assemblyCode = aw.WriteFunction()
+		case "C_RETURN":
+			assemblyCode = aw.WriteReturn()
+		case "C_CALL":
+			assemblyCode, equalityInc = aw.WriteCall(equalityCheckCount)
 		}
 
 		equalityCheckCount += equalityInc
--- projects/vm/translater/translater.go
+++ projects/vm/translater/translater.go
@@ -1,6 +1,9 @@
 package translater
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 type Translater interface {
 	WriteArithmetic(equalityCheckCount int) (string, int)
@@ -8,18 +11,23 @@ type Translater interface {
 	WriteLabel() string
 	WriteGoto() string
 	WriteIf() string
+	WriteFunction() string
+	WriteReturn() string
+	WriteCall(equalityCheckCount int) (string, int)
+	WriteInit() string
 }
 
 type AssemblyWriter struct {
-	Filename    string
-	CommandType string
-	Arg1        string
-	Arg2        int
+	Filename     string
+	FunctionName string
+	CommandType  string
+	Arg1         string
+	Arg2         int
 }
 
 func (aw *AssemblyWriter) WriteArithmetic(equalityCheckCount int) (string, int) {
 	var assemblyCode string
-	e := equalityCheckCount
+	e := "$" + aw.FunctionName + strconv.Itoa(equalityCheckCount)
 	switch aw.Arg1 {
 	case "add":
 		assemblyCode = `@SP
@@ -52,18 +60,18 @@ A=M
 D=M
 A=A-1
 D=D-M
-@EQUAL%d
+@EQUAL%s
 D;JEQ
 @SP
 A=M-1
 M=0
-@END%d
+@END%s
 0;JMP
-(EQUAL%d)
+(EQUAL%s)
 @SP
 A=M-1
 M=-1
-(END%d)
+(END%s)
 `, e, e, e, e)
 		return assemblyCode, 1
 	case "gt":
@@ -73,18 +81,18 @@ A=M
 D=M
 A=A-1
 D=M-D
-@GREATER%d
+@GREATER%s
 D;JGT
 @SP
 A=M-1
 M=0
-@END%d
+@END%s
 0;JMP
-(GREATER%d)
+(GREATER%s)
 @SP
 A=M-1
 M=-1
-(END%d)	
+(END%s)	
 `, e, e, e, e)
 		return assemblyCode, 1
 	case "lt":
@@ -94,18 +102,18 @@ A=M
 D=M
 A=A-1
 D=M-D
-@LESS%d
+@LESS%s
 D;JLT
 @SP
 A=M-1
 M=0
-@END%d
+@END%s
 0;JMP
-(LESS%d)
+(LESS%s)
 @SP
 A=M-1
 M=-1
-(END%d)
+(END%s)
 `, e, e, e, e)
 		return assemblyCode, 1
 	case "and":
@@ -357,15 +365,15 @@ M=D
 }
 
 func (aw *AssemblyWriter) WriteLabel() string {
-	assemblyCode := fmt.Sprintf(`(%s.tempfunction$%s)
-`, aw.Filename, aw.Arg1)
+	assemblyCode := fmt.Sprintf(`(%s$%s)
+`, aw.FunctionName, aw.Arg1)
 	return assemblyCode
 }
 
 func (aw *AssemblyWriter) WriteGoto() string {
-	assemblyCode := fmt.Sprintf(`@%s.tempfunction$%s
+	assemblyCode := fmt.Sprintf(`@%s$%s
 0;JMP	
-`, aw.Filename, aw.Arg1)
+`, aw.FunctionName, aw.Arg1)
 	return assemblyCode
 }
 
@@ -374,8 +382,172 @@ func (aw *AssemblyWriter) WriteIf() string {
 M=M-1
 A=M
 D=M
-@%s.tempfunction$%s
+@%s$%s
 D;JNE
-`, aw.Filename, aw.Arg1)
+`, aw.FunctionName, aw.Arg1)
 	return assemblyCode
+}
+
+func (aw *AssemblyWriter) WriteFunction() string {
+	assemblyCode := fmt.Sprintf(`(%s)
+`, aw.Arg1)
+
+	for val := 0; val < aw.Arg2; val++ {
+		if val == 0 {
+			assemblyCode += `@LCL
+A=M
+M=0
+@SP
+A=M
+M=0
+@SP
+M=M+1
+`
+		} else {
+			assemblyCode += fmt.Sprintf(`@LCL
+D=M
+@%d
+A=D+A
+M=0
+@SP
+A=M
+M=0
+@SP
+M=M+1
+`, aw.Arg2)
+		}
+	}
+
+	return assemblyCode
+}
+
+func (aw *AssemblyWriter) WriteReturn() string {
+
+	restoreMemSegment := func(memorySegment string) string {
+		var decrementValue string
+		switch memorySegment {
+		case "RET":
+			decrementValue = "5"
+		case "LCL":
+			decrementValue = "4"
+		case "ARG":
+			decrementValue = "3"
+		case "THIS":
+			decrementValue = "2"
+		case "THAT":
+			decrementValue = "1"
+		}
+
+		var assemblyCode string
+		if memorySegment == "RET" {
+			assemblyCode = fmt.Sprintf(`@R14
+D=M
+@%s
+A=D-A
+D=M
+@R15
+M=D		
+`, decrementValue)
+		} else {
+			assemblyCode = fmt.Sprintf(`@R14
+D=M
+@%s
+A=D-A
+D=M
+@%s
+M=D		
+`, decrementValue, memorySegment)
+		}
+
+		return assemblyCode
+	}
+
+	setFrame := `@LCL
+D=M
+@R14
+M=D
+`
+	setRet := restoreMemSegment("RET")
+	popToArg := `@SP
+A=M-1
+D=M
+@ARG
+A=M
+M=D
+`
+	restoreSp := `@ARG
+D=M+1
+@SP
+M=D	
+`
+	restoreThat := restoreMemSegment("THAT")
+	restoreThis := restoreMemSegment("THIS")
+	restoreArg := restoreMemSegment("ARG")
+	restoreLcl := restoreMemSegment("LCL")
+	jumpToRet := `@R15
+A=M
+0;JMP
+`
+
+	return setFrame + setRet + popToArg + restoreSp + restoreThat + restoreThis + restoreArg + restoreLcl + jumpToRet
+}
+
+func (aw *AssemblyWriter) WriteCall(equalityCheckCount int) (string, int) {
+	pushVariable := func(variable string) string {
+		return fmt.Sprintf(`@%s
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+`, variable)
+	}
+
+	pushReturn := fmt.Sprintf(`@RETURN.%s.%d
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+`, aw.Arg1, equalityCheckCount)
+	pushLcl := pushVariable("LCL")
+	pushArg := pushVariable("ARG")
+	pushThis := pushVariable("THIS")
+	pushThat := pushVariable("THAT")
+
+	repositionArgVal := fmt.Sprintf("%d", aw.Arg2+5)
+	repositionArg := fmt.Sprintf(`@SP
+D=M
+@%s
+D=D-A
+@ARG
+M=D
+`, repositionArgVal)
+
+	repositionLcl := `@SP
+D=M
+@LCL
+M=D
+`
+
+	gotoFunc := fmt.Sprintf(`@%s
+0;JMP	
+`, aw.Arg1)
+
+	returnLabel := fmt.Sprintf("(RETURN.%s.%d)\n", aw.Arg1, equalityCheckCount)
+
+	return pushReturn + pushLcl + pushArg + pushThis + pushThat + repositionArg + repositionLcl + gotoFunc + returnLabel, 1
+}
+
+func (aw *AssemblyWriter) WriteInit() string {
+	setSp := `@256
+D=A
+@0
+M=D
+`
+	callInit, _ := aw.WriteCall(0)
+
+	return setSp + callInit
 }
