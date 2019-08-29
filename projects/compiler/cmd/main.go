b8ad874c8005fa375c77a6aad604c566c801f2eb
Matthew Kennedy <kennedymj97@gmail.com>
2019-08-29 13:00:32 +0100

    tokenizer complete

--- projects/compiler/cmd/main.go
+++ projects/compiler/cmd/main.go
@@ -7,12 +7,31 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func main() {
 	args := os.Args[1:]
 
 	path := filepath.Clean(args[0])
+
+	outPath := strings.Replace(path, ".jack", "T(gen).xml", 1)
+	outFile, err := os.Create(outPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := outFile.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	w := bufio.NewWriter(outFile)
+	defer func() {
+		if err := w.Flush(); err != nil {
+			log.Fatal(err)
+		}
+	}()
 
 	file, err := os.Open(path)
 	if err != nil {
@@ -24,11 +43,45 @@ func main() {
 		}
 	}()
 
+	// Create output xml file
+
 	scanner := bufio.NewScanner(file)
 	scanner.Split(tokenizer.ScanTokens)
-
+	w.WriteString(`<tokens>
+`)
 	for scanner.Scan() {
-		text := scanner.Text()
-		fmt.Println(text)
+		token := scanner.Text()
+		tokenType := tokenizer.TokenType(token)
+		if tokenType == "STRING_CONST" {
+			token = token[1 : len(token)-1]
+		}
+		switch token {
+		case "<":
+			token = "&lt;"
+		case ">":
+			token = "&gt;"
+		case "&":
+			token = "&amp;"
+		case `"`:
+			token = "&quot;"
+		}
+		var tokenLabel string
+		switch tokenType {
+		case "KEYWORD":
+			tokenLabel = "keyword"
+		case "SYMBOL":
+			tokenLabel = "symbol"
+		case "STRING_CONST":
+			tokenLabel = "stringConstant"
+		case "INT_CONST":
+			tokenLabel = "integerConstant"
+		case "IDENTIFIER":
+			tokenLabel = "identifier"
+		default:
+			tokenLabel = ""
+		}
+		w.WriteString(fmt.Sprintf(`<%s>%s</%s>
+`, tokenLabel, token, tokenLabel))
 	}
+	w.WriteString(`</tokens>`)
 }
--- projects/compiler/tokenizer/tokenizer.go
+++ projects/compiler/tokenizer/tokenizer.go
@@ -2,50 +2,93 @@ package tokenizer
 
 import (
 	"bytes"
+	"strconv"
+	"unicode"
 	"unicode/utf8"
 )
 
-// ScanTokens is a split function for a Scanner that returns each token in a jack file.
-func ScanTokens(data []byte, atEOF bool) (advance int, token []byte, err error) {
-	// Skip leading spaces.
-	var r rune
-	var width int
-	start := 0
-	for width = 0; start < len(data); start += width {
-		r, width = utf8.DecodeRune(data[start:])
-		if !isSpace(r) {
-			break
+func isKeyword(token string) bool {
+	keywords := []string{
+		"class",
+		"method",
+		"function",
+		"constructor",
+		"int",
+		"boolean",
+		"char",
+		"void",
+		"var",
+		"static",
+		"field",
+		"let",
+		"do",
+		"if",
+		"else",
+		"while",
+		"return",
+		"true",
+		"false",
+		"null",
+		"this",
+	}
+
+	for _, keyword := range keywords {
+		if token == keyword {
+			return true
 		}
 	}
 
-	// NEED A WAY TO PARSE OUT COMMENTS AND PARSE OUT STRINGS
-
-	r, width = utf8.DecodeRune(data[start:])
-
-	if isSymbol(r) {
-		return start + width, data[start : start+width], nil
-	}
-
-	// Scan until space, marking end of word.
-	for width, i := 0, start; i < len(data); i += width {
-		r, width = utf8.DecodeRune(data[i:])
-		if isSpace(r) || isSymbol(r) {
-			return i, data[start:i], nil
-		}
-	}
-	// If we're at EOF, we have a final, non-empty, non-terminated word. Return it.
-	if atEOF && len(data) > start {
-		return len(data), data[start:], nil
-	}
-	// Request more data.
-	return start, nil, nil
+	return false
 }
 
-func removeComment(data []byte) []byte {
-	if i := bytes.IndexByte(data, '\n'); i >= 0 {
-		return data[i+1 : len(data)]
+func isInt(token string) bool {
+	if _, err := strconv.Atoi(token); err == nil {
+		return true
 	}
-	return data
+	return false
+}
+
+func isIdentifier(token string) bool {
+	if isInt(string(token[0])) {
+		return false
+	}
+	for _, char := range token {
+		if !unicode.IsDigit(char) && !unicode.IsLetter(char) && char != '_' {
+			return false
+		}
+	}
+	return true
+}
+
+func TokenType(token string) string {
+	if isKeyword(token) {
+		return "KEYWORD"
+	} else if isSymbol(rune(token[0])) {
+		return "SYMBOL"
+	} else if isStringConstant(rune(token[0])) {
+		return "STRING_CONST"
+	} else if isInt(token) {
+		return "INT_CONST"
+	} else if isIdentifier(token) {
+		return "IDENTIFIER"
+	}
+	return ""
+}
+
+func removeComment(data []byte) (bool, int) {
+	var width int
+	if string(data[:2]) == "//" {
+		width = bytes.IndexByte(data, '\n') + 1
+		return true, width
+	} else if string(data[:3]) == "/**" {
+		width = bytes.Index(data, []byte("*/")) + 2
+		return true, width
+	}
+	return false, -1
+}
+
+func isStringConstant(r rune) bool {
+	return r == '"'
 }
 
 func isSymbol(r rune) bool {
@@ -79,4 +122,46 @@ func isSpace(r rune) bool {
 		return true
 	}
 	return false
+}
+
+// ScanTokens is a split function for a Scanner that returns each token in a jack file.
+func ScanTokens(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	var r rune
+	var width int
+	start := 0
+	// Skip leading spaces and comments.
+	for width = 0; start < len(data); start += width {
+		r, width = utf8.DecodeRune(data[start:])
+		// If the first 2 runes are // then find where the end of the line is and shift the start value
+		if isComment, commentWidth := removeComment(data[start:]); isComment {
+			width = commentWidth
+		} else if !isSpace(r) {
+			break
+		}
+	}
+
+	// If the first rune is a symbol or string constant return it
+	r, width = utf8.DecodeRune(data[start:])
+	if isSymbol(r) {
+		return start + width, data[start : start+width], nil
+	} else if isStringConstant(r) {
+		constantEndIndex := bytes.IndexByte(data[start+1:], '"') + 2
+		return start + constantEndIndex, data[start : start+constantEndIndex], nil
+	}
+
+	// Scan until space or comment, marking end of word.
+	for width, i := 0, start; i < len(data); i += width {
+		r, width = utf8.DecodeRune(data[i:])
+		if isSpace(r) || isSymbol(r) {
+			return i, data[start:i], nil
+		}
+	}
+
+	// If we're at EOF, we have a final, non-empty, non-terminated word. Return it.
+	if atEOF && len(data) > start {
+		return len(data), data[start:], nil
+	}
+
+	// Request more data.
+	return start, nil, nil
 }
